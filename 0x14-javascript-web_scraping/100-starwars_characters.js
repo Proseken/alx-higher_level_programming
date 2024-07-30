dd56b3afbf36d16e8864a47bfab83593f4f6ecc1
Proseken <sundaymiracle0010@gmail.com>
2024-07-30 05:34:18 -0700

    Advance task 1

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,24 +1,28 @@
 #!/usr/bin/node
-//
-const request = require('request');
-const movieId = process.argv[2];
-const apiUrl = 'https://swapi-api.altx-tools.com/api/films/'.concat(movieId);
 
-request(apiUrl, (error, response, body) => {
+const request = require('request');
+
+if (process.argv.length !== 3) {
+  process.exit();
+}
+
+const id = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${id}/`;
+
+request(url, function (error, response, body) {
   if (error) {
-    console.error(error);
-    return;
+    console.log(error);
+  } else if (response.statusCode === 200) {
+    const characters = JSON.parse(body).characters;
+    for (const data of characters) {
+      request(data, function (error, response, body) {
+        if (error) {
+          console.log(error);
+        } else if (response.statusCode === 200) {
+          const character = JSON.parse(body);
+          console.log(character.name);
+        }
+      });
+    }
   }
-  const movie = JSON.parse(body);
-  const charactersUrls = movie.characters;
-  charactersUrls.forEach((characterUrl) => {
-    request.get(characterUrl, (error, response, body) => {
-      if (error) {
-        console.error(error);
-        return;
-      }
-      const character = JSON.parse(body);
-      console.log(character.name);
-    });
-  });
 });
